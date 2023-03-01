b495d0f3bc98439976859b72178089db6814d68b
ggooltae <waterboy94@naver.com>
2023-03-01 23:31:49 +0900

    feat: bar차트 툴팁 적용

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { select } from 'd3-selection';
+import { pointer, select } from 'd3-selection';
 import { scaleLinear, scaleBand } from 'd3-scale';
 import { axisLeft } from 'd3-axis';
 import 'd3-transition';
@@ -9,15 +9,16 @@ import styled from 'styled-components';
 import WidgetHeader from './WidgetHeader';
 import ErrorBoundary from './ErrorBoundary';
 
+import api from '../api';
 import type { SpotData } from '../config/types';
 import { IGridContainer } from '../config/interfaces';
 
-import { MESSAGE, TIME } from '../config/constants';
+import { MESSAGE, TIME, COLOR } from '../config/constants';
 
 interface IBarChart {
   title: string;
   gridArea: string;
-  chartData: SpotData;
+  chartData: SpotData[];
   isPaused: boolean;
   isError: boolean;
   errorCount: number;
@@ -36,6 +37,7 @@ function BarChart({
   resumeInterval,
 }: IBarChart) {
   const svgRef = useRef<SVGSVGElement>(null);
+  const tooltipRef = useRef(null);
   const [svgWidth, setSvgWidth] = useState(0);
   const [svgHeight, setSvgHeight] = useState(0);
 
@@ -58,48 +60,52 @@ function BarChart({
 
   useEffect(() => {
     const svg = select(svgRef.current);
+
+    const tooltip = select(tooltipRef.current);
     const margin = { top: 20, right: 20, bottom: 0, left: 50 };
 
-    const keys = Object.keys(chartData);
-    const values = Object.values(chartData);
+    const keyArray = chartData.map((spotData) => spotData.key);
+    const dataArray = chartData.map((spotData) => Number(spotData.data));
 
     const xScale = scaleLinear()
-      .domain([0, Math.max(...values)])
+      .domain([0, Math.max(...dataArray)])
       .range([0, svgWidth - margin.left - margin.right - 20]);
     const yScale = scaleBand()
-      .domain(keys.map((key) => key.replace(/\w+_/, '').toUpperCase()))
+      .domain(keyArray.map((key) => key.replace(/\w+_/, '').toUpperCase()))
       .range([margin.top, svgHeight - margin.bottom])
       .padding(0.1);
 
+    tooltip.style('opacity', 0);
+
     svg
       .selectAll('rect')
-      .data(values)
+      .data(chartData)
       .join('rect')
       .attr('x', margin.left)
       .attr(
         'y',
-        (_, i) => yScale(keys[i].replace(/\w+_/, '').toUpperCase()) || 0,
+        (_, i) => yScale(keyArray[i].replace(/\w+_/, '').toUpperCase()) || 0,
       )
-      .attr('fill', 'steelblue')
+      .attr('fill', COLOR.BLUE)
       .attr('height', yScale.bandwidth())
       .transition()
-      .duration(1000)
-      .attr('width', (d) => xScale(d));
+      .duration(500)
+      .attr('width', (d) => xScale(d.data));
 
     svg
       .selectAll('.value-text')
-      .data(values)
+      .data(dataArray)
       .join('text')
       .attr('class', 'value-text')
       .attr(
         'y',
         (_, i) =>
-          (yScale(keys[i].replace(/\w+_/, '').toUpperCase()) || 0) +
+          (yScale(keyArray[i].replace(/\w+_/, '').toUpperCase()) || 0) +
           yScale.bandwidth() / 2,
       )
       .attr('dy', '0.35em')
       .transition()
-      .duration(1000)
+      .duration(500)
       .text((d) => d)
       .attr('x', (d) => xScale(d) + margin.left + 5);
 
@@ -107,6 +113,32 @@ function BarChart({
       .select<SVGGElement>('#axisY')
       .attr('transform', `translate(${margin.left},0)`)
       .call(axisLeft(yScale));
+
+    svg
+      .selectAll<SVGRectElement, SpotData>('rect')
+      .on('mousemove', function (event, d) {
+        const [posX, posY] = pointer(event);
+
+        select(this).attr('fill', COLOR.HOVER);
+        tooltip
+          .html(`${api.OPEN_API[''][d.key]}`)
+          .style('left', `${posX + 10}px`)
+          .style('top', `${posY + 10}px`)
+          .style('opacity', 0.8);
+      })
+      .on('mouseover', function () {
+        tooltip.style('opacity', 0.8);
+      })
+      .on('mouseout', function () {
+        select(this).attr('fill', COLOR.BLUE);
+        tooltip.style('opacity', 0);
+      });
+
+    return () => {
+      svg.selectAll('rect').on('mousemove', null);
+      svg.selectAll('rect').on('mouseover', null);
+      svg.selectAll('rect').on('mouseout', null);
+    };
   }, [chartData, svgWidth, svgHeight]);
 
   return (
@@ -122,9 +154,12 @@ function BarChart({
           message={`${MESSAGE.FETCH_ERROR} (재시도 횟수: ${errorCount})`}
         />
       ) : (
-        <SVG ref={svgRef}>
-          <g id="axisY" />
-        </SVG>
+        <Chart>
+          <SVG ref={svgRef}>
+            <g id="axisY" />
+          </SVG>
+          <Tooltip ref={tooltipRef} />
+        </Chart>
       )}
     </Container>
   );
@@ -141,8 +176,23 @@ const Container = styled.div<IGridContainer>`
   background-color: white;
 `;
 
+const Chart = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
 const SVG = styled.svg`
   width: 100%;
+`;
+
+const Tooltip = styled.div`
+  position: absolute;
+  text-align: center;
+  font-size: 0.8rem;
+  background-color: yellow;
+  border-radius: 5px;
+  padding: 0.5rem;
+  opacity: 0;
 `;
 
 export default React.memo(BarChart);
