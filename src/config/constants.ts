97eee97a44dd8cdd5b0dd53cf591e62cced3fcbf
ggooltae <waterboy94@naver.com>
2023-03-04 20:54:24 +0900

    refactor: contant객체 Object.freeze() 적용

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,15 +1,15 @@
-export const TIME = {
+export const TIME = Object.freeze({
   SECOND: 1000,
   MINUTE: 1000 * 60,
   HOUR: 1000 * 60 * 60,
   RESIZE_DEBOUNCE_TIME: 500,
-};
+});
 
-export const MESSAGE = {
+export const MESSAGE = Object.freeze({
   FETCH_ERROR: 'fetch 에러 발생, 5초 간격으로 재시도합니다.',
-};
+});
 
-export const COLOR = {
+export const COLOR = Object.freeze({
   BLUE: '#0066FF',
   HOVER: '#FFA96A',
-};
+});
